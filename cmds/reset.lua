b4f9d651acecaa03c0e010a80dd39e100cccbc07
scriptdumpkll <113224535+scriptdumpkll@users.noreply.github.com>
2022-09-24 22:14:46 +0800

    Create reset.lua

---
+++ cmds/reset.lua
@@ -0,0 +1,14 @@
+return function()
+    for i,v in pairs(game:service"Players".LocalPlayer.Character:GetChildren()) do
+        if v:IsA("MeshPart") or v.Name:lower():find("humanoid") then
+            v:Destroy()
+        end
+    end
+end
+return function()
+    for i,v in pairs(game:service"Players".LocalPlayer.Character:GetChildren()) do
+        if v:IsA("MeshPart") or v.Name:lower():find("humanoid") then
+            v:Destroy()
+        end
+    end
+end
